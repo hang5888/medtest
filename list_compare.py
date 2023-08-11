f063233b3a82b9fb24a9433e705df6c1427c2ed1
hang5888 <72378336+hang5888@users.noreply.github.com>
2023-08-11 16:10:10 +0800

    Create list_compare.py

---
+++ list_compare.py
@@ -0,0 +1,6 @@
+def list_str(data,what_str):
+  erer=[sublist for sublist in data if what_str in sublist]
+  return erer
+def list_str(data,what_str):
+  erer=[sublist for sublist in data if what_str not in sublist]
+  return erer
